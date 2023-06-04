2dbbca1b557a81bd39c8872bee1c018470dc5416
Adonay <AH17010@ues.edu.sv>
2023-06-04 11:27:31 -0600

    refactor de services

--- src/main/java/com/ues/bibliotecalibro/services/BibliotecaService.java
+++ src/main/java/com/ues/bibliotecalibro/services/BibliotecaService.java
@@ -1,15 +1,26 @@
 package com.ues.bibliotecalibro.services;
 
 import com.ues.bibliotecalibro.entity.Biblioteca;
+import com.ues.bibliotecalibro.entity.Libro;
 import com.ues.bibliotecalibro.responseDto.BibliotecaResponseDto;
 
+import com.ues.bibliotecalibro.responseDto.GenericResponse;
+import com.ues.bibliotecalibro.responseDto.LibroRequestDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 
 
-public interface BibliotecaService extends ICRUD<Biblioteca>{
+public interface BibliotecaService {
 
     ResponseEntity<List<BibliotecaResponseDto>> ListaBibliotecas();
+
+    ResponseEntity<GenericResponse> registrar(LibroRequestDto obj);
+    ResponseEntity<GenericResponse> modificar (LibroRequestDto obj,Integer id);
+    ResponseEntity<Page<Libro>> listar(Pageable pageable);
+    Biblioteca leerPorId(Integer id);
+    ResponseEntity<GenericResponse>  eliminar(Integer id);
 
 }
--- src/main/java/com/ues/bibliotecalibro/services/ILibroService.java
+++ src/main/java/com/ues/bibliotecalibro/services/ILibroService.java
@@ -1,6 +1,8 @@
 package com.ues.bibliotecalibro.services;
 
 import com.ues.bibliotecalibro.entity.Libro;
+import com.ues.bibliotecalibro.responseDto.GenericResponse;
+import com.ues.bibliotecalibro.responseDto.LibroRequestDto;
 import com.ues.bibliotecalibro.responseDto.LibrosResponseDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -9,12 +11,16 @@ import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 
-public interface ILibroService extends ICRUD<Libro>{
+public interface ILibroService {
 
     ResponseEntity<Page<LibrosResponseDto>> ListaLibros(Pageable pageable);
 
     ResponseEntity<Libro> buscarByAny(Integer id);
 
-
+    ResponseEntity<GenericResponse> registrar(LibroRequestDto obj);
+    ResponseEntity<GenericResponse> modificar (LibroRequestDto obj,Integer id);
+    ResponseEntity<Page<Libro>> listar(Pageable pageable);
+    Libro leerPorId(Integer id);
+    ResponseEntity<GenericResponse>  eliminar(Integer id);
 
 }
